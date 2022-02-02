914191c9123f2ff83eb02a4bd69af3a241198728
Hayagreeve <98875485+Hayagreeve@users.noreply.github.com>
2022-02-02 20:54:31 +0530

    #include<stdio.h> int main() {     float a,b;     float c;     scanf("%f%f",&a,&b);     if(1<=a,b<=1000)     {         c=(a+b)/2;         printf("%.4f",c);
        }
        return 0;
    }

---
+++ Find_the_average_of_two_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    float c;
+    scanf("%f%f",&a,&b);
+    if(1<=a,b<=1000)
+    {
+        c=(a+b)/2;
+        printf("%.4f",c);
+        
+    }
+    return 0;
+}
